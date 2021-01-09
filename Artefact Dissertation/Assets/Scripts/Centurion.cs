7a48136a6e09990b6e9d7e4a542f3e3e3bf498dd
DenisSRemo <43639388+DenisSRemo@users.noreply.github.com>
2021-01-09 17:31:54 +0000

    commit

---
+++ Artefact Dissertation/Assets/Scripts/Centurion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Centurion : agent
+{
+    
+    public Formation formation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        formation = Formation.Open_Order;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Artefact Dissertation/Assets/Scripts/Legionary.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Legionary : agent
+{
+   
+    [SerializeField] private Centurion centurion;
+    [SerializeField] private float Rank;
+    [SerializeField] private float Column;
+    public Vector3 pos;
+    public float distanceOpenOrder=10f;
+
+
+
+    NavMeshAgent agent;
+
+
+
+
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        pos = centurion.transform.position;
+        pos.x = pos.x + Column * distanceOpenOrder;
+        pos.z = pos.z + Rank * -distanceOpenOrder;
+        agent.destination = pos;
+       
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                pos = hit.point;
+                pos.x = pos.x + Column * distanceOpenOrder;
+                pos.z = pos.z + Rank * -distanceOpenOrder;
+                agent.destination = pos;
+            }
+            
+        }
+
+    }
+
+    protected void Open_Order()
+    {
+
+    }
+    protected void Close_Order()
+    {
+
+    }
+    protected void Testudo()
+    {
+
+    }
+    protected void Orbis()
+    {
+
+    }
+
+    protected void Cuneus()
+    {
+
+    }
+}
