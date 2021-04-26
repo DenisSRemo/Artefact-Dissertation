d1e051e435638ec17babb47eb9dd485d0b98461a
DenisSRemo <43639388+DenisSRemo@users.noreply.github.com>
2021-04-26 15:22:17 +0100

    artefact tested and ready for word count evaluation

--- Artefact Dissertation/Assets/Scripts/CameraController.cs
+++ Artefact Dissertation/Assets/Scripts/CameraController.cs
@@ -24,19 +24,19 @@ public class CameraController : MonoBehaviour
         Vector3 pos = transform.position;
         if(Input.GetKey("w"))//||Input.mousePosition.y>=Screen.height-panBorderThickness)
         {
-            pos.z += panSpeed * Time.deltaTime;
+            pos.z -= panSpeed * Time.deltaTime;
         }
         if (Input.GetKey("s"))// || Input.mousePosition.y <=  panBorderThickness)
         {
-            pos.z -= panSpeed * Time.deltaTime;
+            pos.z += panSpeed * Time.deltaTime;
         }
         if (Input.GetKey("d") )//|| Input.mousePosition.x >= Screen.width - panBorderThickness)
         {
-            pos.x += panSpeed * Time.deltaTime;
+            pos.x -= panSpeed * Time.deltaTime;
         }
         if (Input.GetKey("a") )//|| Input.mousePosition.x <= panBorderThickness)
         {
-            pos.x -= panSpeed * Time.deltaTime;
+            pos.x += panSpeed * Time.deltaTime;
         }
 
 
